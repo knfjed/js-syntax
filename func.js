060f7ac9792c0d4a5a631a4af8fc9ecd81effff0
knfjed <knfjed@gmail.com>
2020-01-22 04:49:21 +0900

    add homework No.1-4

--- func.js
+++ func.js
@@ -1,1 +1,27 @@
-console.log("hello");
+// ①引数２つの値を足算した値を返却する関数
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+const sum = add(1, 1);
+console.log(sum);
+
+// ②引数２つの値を引算した値を返却する関数
+const sub = (number1, number2) => {
+  return number1 - number2;
+};
+const total = sub(10, 1);
+console.log(total);
+
+// ③引数２つの値を割り算した値を返却する関数
+const div = (number1, number2) => {
+  return number1 / number2;
+};
+const all = div(10, 1);
+console.log(all);
+
+// ④引数２つの値を掛け算した値を返却する関数
+const multi = (number1, number2) => {
+  return number1 * number2;
+};
+const amount = multi(5, 5);
+console.log(amount);
